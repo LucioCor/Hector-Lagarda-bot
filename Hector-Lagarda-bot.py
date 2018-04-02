3447a6e9a36511e4db0bda31d82c40aa46e6b8b1
Lucio Corujo <joselucio_corujo95@hotmail.com>
2018-04-01 17:14:49 -0700

    Update Hector-Lagarda-bot.py

--- Hector-Lagarda-bot.py
+++ Hector-Lagarda-bot.py
@@ -10,6 +10,14 @@ from discord.ext import commands
 
 g = safygiphy.Giphy()
 
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    discord.opus.load_opus('opus_lib')
+
 class VoiceEntry:
     def __init__(self, message, player):
         self.requester = message.author
