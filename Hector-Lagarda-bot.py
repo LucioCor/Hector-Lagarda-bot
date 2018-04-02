7168b11dfb71a1ac2aec6bcff3f8ec32d66f95e6
Lucio Corujo <joselucio_corujo95@hotmail.com>
2018-04-02 16:42:15 -0700

    Update Hector-Lagarda-bot.py

--- Hector-Lagarda-bot.py
+++ Hector-Lagarda-bot.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 g = safygiphy.Giphy()
 
 if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
+    discord.opus.load_opus('opus.py')
 
 class VoiceEntry:
     def __init__(self, message, player):
