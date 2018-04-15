e8390653ad42e420aad4b58c2dbf5e9a06ffd6a5
Lucio Corujo <joselucio_corujo95@hotmail.com>
2018-04-15 15:00:18 -0700

    Update Hector-Lagarda-bot.py

--- Hector-Lagarda-bot.py
+++ Hector-Lagarda-bot.py
@@ -9,7 +9,7 @@ import requests
 from discord.ext import commands
 
 from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['TOKEN'])
+conn = S3Connection(os.environ['TOKEN'])
 
 g = safygiphy.Giphy()
 voz = True
